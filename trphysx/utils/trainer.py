d0300489e886e925fc7d26771acddf1c9ff026d5
joshwatso <74628797+joshwatso@users.noreply.github.com>
2023-09-09 23:00:38 +0100

    Update trainer.py

--- trphysx/utils/trainer.py
+++ trphysx/utils/trainer.py
@@ -244,7 +244,7 @@ class Trainer:
 
             if epoch % 25 == 0:
                 self.args.train_batch_size = self.args.train_batch_size * 2
-                logger.info(f"Changing batch size to {new_batch_size} at epoch {epoch}")
+                #logger.info(f"Changing batch size to {new_batch_size} at epoch {epoch}")
                 logger.info(f"Current batch size after update: {training_loader.batch_size}")
 
 
