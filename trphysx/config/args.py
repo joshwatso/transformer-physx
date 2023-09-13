6eac0c6199e452901cba827381250546699f6b8a
joshwatso <74628797+joshwatso@users.noreply.github.com>
2023-09-13 16:12:06 +0100

    epochs = 10

--- trphysx/config/args.py
+++ trphysx/config/args.py
@@ -108,7 +108,7 @@ class TrainingArguments:
         default=0, metadata={"help": "Epoch to start training at"}
     )
     epochs: int = field(
-        default=200, metadata={"help": "Number of epochs to train"}
+        default=10, metadata={"help": "Number of epochs to train"}
     )
 
     # ===== Optimization parameters =====
